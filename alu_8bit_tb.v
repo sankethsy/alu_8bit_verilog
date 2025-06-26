4eedbbda8164293a59f3b31c5dc56473e3e96bf6
sankethsy <sankethsanketh89@gmail.com>
2025-06-26 22:23:57 +0530

    Update alu_8bit_tb.v

--- alu_8bit_tb.v
+++ alu_8bit_tb.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 25.06.2025 00:26:50
-// Design Name: 
-// Module Name: alu_8bit_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module alu_8bit_tb;
  reg [7:0] A,B;
  reg [3:0] opcode;
